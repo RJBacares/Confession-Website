ae411613b875732393564728a95dba5d03b99f99
RJBacares <rjbacares.2004@gmail.com>
2024-12-19 01:13:38 +0800

    Update Notification.php

--- Notification.php
+++ Notification.php
@@ -1,11 +1,45 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-require 'vendor/autoload.php';
+date_default_timezone_set('Etc/UTC');
 
-$to = "notification43069@gmail.com";
-$subject = "Selected Date";
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer();
+
+$mail->isSMTP();
+
+$mail->SMTPDebug = SMTP::DEBUG_CONNECTION;
+
+$mail->Host = 'smtp.example.com';
+
+$mail->Port = 465;
+
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+
+$mail->SMTPOptions = array(
+    'ssl' => [
+        'verify_peer' => true,
+        'verify_depth' => 3,
+        'allow_self_signed' => true,
+        'peer_name' => 'smtp.example.com',
+        'cafile' => '/etc/ssl/ca_cert.pem',
+    ],
+);
+
+
+$mail->SMTPAuth = true;
+
+$mail->Username = 'serverside42069@gmail.com';
+
+$mail->Password = 'yrwy wtlt gpsv emcx';
+
+$mail->setFrom('serverside42069@gmail.com', 'Site Notification');
+
+$mail->addAddress('notification43069@gmail.com', 'Site Notification');
+
+$mail->Subject = 'Selected Date';
 
 if (isset($_GET['date'])) {
     $date = $_GET['date'];
@@ -19,27 +53,11 @@ if (isset($_GET['date'])) {
     $message = "No date selected.";
 }
 
-$mail = new PHPMailer(true);
+$mail->Body = $message;
 
-try {
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'serverside42069@gmail.com';
-    $mail->Password = 'yrwy wtlt gpsv emcx';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
-
-    $mail->setFrom('serverside42069@gmail.com', 'Notification');
-    $mail->addAddress($to);
-
-    $mail->isHTML(false);
-    $mail->Subject = $subject;
-    $mail->Body = $message;
-
-    $mail->send();
-    echo "Mail sent successfully!";
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
 }
 ?>
