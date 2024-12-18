3894327827a5d2d9984ffe60c1f0e71f35efe616
RJBacares <rjbacares.2004@gmail.com>
2024-12-18 20:58:32 +0800

    Update Notification.php

--- Notification.php
+++ Notification.php
@@ -27,10 +27,9 @@ try {
     $mail->SMTPAuth = true;
     $mail->Username = 'serverside42069@gmail.com';
     $mail->Password = 'yrwy wtlt gpsv emcx'; 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465; // SSL port
 
-    
     $mail->setFrom('serverside42069@gmail.com', 'Notification');
     $mail->addAddress($to);
 
