68880b8bd64166e50f54c2713b666b60890ef0eb
RJBacares <rjbacares.2004@gmail.com>
2024-12-19 08:26:24 +0800

    d
    ss

--- Notification.php
+++ Notification.php
@@ -9,25 +9,16 @@ require '../vendor/autoload.php';
 $mail = new PHPMailer();
 
 $mail->isSMTP();
-
-$mail->SMTPDebug = SMTP::DEBUG_SERVER; 
-
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 $mail->Host = 'smtp.gmail.com';
-
 $mail->Port = 465;
-
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; 
-
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 $mail->SMTPAuth = true;
-
 $mail->Username = 'serverside42069@gmail.com';
-
 $mail->Password = 'kgel stwl pakb yuzj';
 
 $mail->setFrom('serverside42069@gmail.com', 'Site Notification');
-
 $mail->addAddress('notification43069@gmail.com', 'Site Notification');
-
 $mail->Subject = 'Selected Date';
 
 if (isset($_GET['date'])) {
@@ -48,5 +39,11 @@ if (!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     echo 'Message sent!';
+}
+
+if (isset($_GET['redirect'])) {
+    $redirectUrl = $_GET['redirect'];
+    header("Location: $redirectUrl");
+    exit();
 }
 ?>
